0bd9d602a947cf0a2903713d126a683f04b21b2b
MauricioSalas <mauricio.salas16@inacapmail.cl>
2022-07-04 21:05:54 -0400

    Agregar Capa DTO (Interfaz y Clases)

---
+++ EventosModel/DAL/AsistentesDALDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventosModel.DAL
+{
+    public class AsistentesDALDB : IAsistentesDAL
+    {
+        public void AgregarAsistente(Asistente asistente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EliminarAsistente(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Asistente Obtener(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Asistente> ObtenerAsistentes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Asistente> ObtenerAsistentes(string estado)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
